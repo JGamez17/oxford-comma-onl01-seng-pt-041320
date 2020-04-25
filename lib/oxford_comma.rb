eaa26ea8038c1240a3174d6257e4fa1df13ba83a
Jessica Gamez <jpgamez1789@icloud.com>
2020-04-25 22:11:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2 
     array.join (" and ") 
   else array.size > 2 
-    array [-1].prepend (" and ")
+    array [-1].prepend ("and ")
     array.join (", ")
   end
 end
