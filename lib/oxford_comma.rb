7d56db7f9f8f95340166db878027badce830173b
Jessica Gamez <jpgamez1789@icloud.com>
2020-04-25 22:08:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2 
     array.join (" and ") 
   else array.size > 2 
-    array [-1].preprend
+    array [-1].preprend (" and ")
     array.join (" , ")
   end
 end
